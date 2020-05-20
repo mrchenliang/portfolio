18aa0f95f988ed66a2dd6074cef04eeaa021966b
mrchenliang <c.liang@ualberta.ca>
2020-05-20 07:29:59 -0400

    Updates

---
+++ precache-manifest.b23f9fa1b1b5080890ad70f3b1412751.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6410ba3ecd101eefd9162755f35042b6",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "bff4692150da4f1d39ad",
+    "url": "/portfolio/static/css/main.0876cdd9.chunk.css"
+  },
+  {
+    "revision": "6bb64744ce24d0b410f1",
+    "url": "/portfolio/static/js/2.18e4ac39.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.18e4ac39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bff4692150da4f1d39ad",
+    "url": "/portfolio/static/js/main.2b80ec57.chunk.js"
+  },
+  {
+    "revision": "88fbd3d397ae3690d5f2",
+    "url": "/portfolio/static/js/runtime-main.fb1b3dac.js"
+  },
+  {
+    "revision": "d9dfa5dbd290c43d8389137815d84680",
+    "url": "/portfolio/static/media/image.d9dfa5db.jpg"
+  }
+]);
