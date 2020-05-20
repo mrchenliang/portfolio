591fe50b46e05bf1b7b93f11ebc04e36b1ff1cfc
mrchenliang <c.liang@ualberta.ca>
2020-05-20 08:32:58 -0400

    change deploy to deployment

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import 'font-awesome/css/font-awesome.min.css';
 import image from './image.jpg';
 import './App.css';
 
@@ -9,6 +10,12 @@ function App() {
         <img src={image} className="App-logo" alt="logo" />
         <p>
           Chen's Personal Portfolio
+          <ul className="Social-Icons">
+            <li><a href="http://instagram.com/mr_chenliang"><i className="fa fa-instagram icon"/></a></li>
+            <li><a href="http://twitter.com/mr_chenliang"><i className="fa fa-twitter icon"/></a></li>
+            <li><a href="http://linkedin.com/in/mrchenliang"><i className="fa fa-linkedin icon"/></a></li>
+            <li><a href="http://github.com/mrchenliang"><i className="fa fa-github icon"/></a></li>
+          </ul>
         </p>
       </header>
     </div>
