ffd0f2cbd0b4916dfe3f96299c90f4615ac43921
mrchenliang <c.liang@ualberta.ca>
2020-05-23 17:04:36 -0400

    fixing layout and adding styling changes for portfolio

--- src/App.js
+++ src/App.js
@@ -7,15 +7,47 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={image} className="App-logo" alt="logo" />
         <p>
-          Chen's Personal Portfolio
+        <div className = 'header'>
+          <img src={image} className="App-logo" alt="logo" />
+          <div className = 'info'>
+            <div>Chen Liang</div>
+            <div>Software Engineer | Full Stack Developer</div>
+            <div> Toronto <span role="img" aria-label="Canada"> 🇨🇦</span></div>
+          </div>
+        </div>
           <ul className="Social-Icons">
-            <li><a href="mailto:c.liang@ualberta.ca"><i className="fa fa-envelope icon"/></a></li>
-            <li><a href="http://instagram.com/mr_chenliang"><i className="fa fa-instagram icon"/></a></li>
-            <li><a href="http://twitter.com/mr_chenliang"><i className="fa fa-twitter icon"/></a></li>
-            <li><a href="http://linkedin.com/in/mrchenliang"><i className="fa fa-linkedin icon"/></a></li>
-            <li><a href="http://github.com/mrchenliang"><i className="fa fa-github icon"/></a></li>
+            <li>
+              <div>
+                <a href="mailto:c.liang@ualberta.ca" rel="noopener noreferrer" target="_blank"><i className="fa fa-envelope icon tooltip"><div className = 'tooltiptext'>Email</div></i>
+                </a>
+              </div>
+            </li>
+            <li>
+              <div>
+                <a href="http://instagram.com/mr_chenliang" rel="noopener noreferrer" target="_blank"><i className="fa fa-instagram icon tooltip"><div className = 'tooltiptext'>Instagram</div></i></a>              
+              </div>
+            </li>
+            <li>
+              <div>
+                <a href="http://twitter.com/mr_chenliang" rel="noopener noreferrer" target="_blank"><i className="fa fa-twitter icon tooltip"><div className = 'tooltiptext'>Twitter</div></i></a>              
+              </div>
+            </li>
+            <li>
+              <div>
+                <a href="http://linkedin.com/in/mrchenliang" rel="noopener noreferrer" target="_blank"><i className="fa fa-linkedin icon tooltip"><div className = 'tooltiptext'>LinkedIn</div></i></a>              
+              </div>
+            </li>
+            <li>
+              <div>
+                <a href="http://github.com/mrchenliang" rel="noopener noreferrer" target="_blank"><i className="fa fa-github icon tooltip"><div className = 'tooltiptext'>Github</div></i></a>              
+              </div>
+            </li>
+            <li>
+              <div>
+                <a href="http://chenliang.ca/resume" rel="noopener noreferrer" target="_blank"><i className="fa fa-file icon tooltip"><div className = 'tooltiptext'>Resume</div></i></a>              
+              </div>
+            </li>
           </ul>
         </p>
         <footer><p>Copyright © Chen Liang 2020</p></footer>
